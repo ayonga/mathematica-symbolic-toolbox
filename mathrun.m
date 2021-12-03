41b709304e61aed168dd5aebc0308704553292e9
Ayonga Hereid <ayonga27@gmail.com>
2021-12-02 23:19:00 -0500

    update script.

--- mathrun.m
+++ mathrun.m
@@ -1,12 +1,33 @@
-path2toolbox = ''; % Fill in: e.g. C:\Users\Spece\OneDrive\Code\MATLAB\SysUtils\mathematica_for_matlab
-path2mathematica = ''; % Fill in: e.g. C:\Program Files\Wolfram Research\Mathematica\11.2
-addpath(path2toolbox);
-addpath(path2mathematica);
+clc;
+clear 
 
 %%
-mllib=fullfile(path2toolbox, 'wstp64i4.lib'); % Copy wstp64i4.lib into toolbox directory or replace wstp64i4 with the appropriate library for your version of Mathematica
+
+% path2toolbox = '/usr/local/Wolfram/Mathematica/12.3/SystemFiles/Links/WSTP/DeveloperKit/Linux-x86-64/CompilerAdditions'; % Fill in: e.g. C:\Users\Spece\OneDrive\Code\MATLAB\SysUtils\mathematica_for_matlab
+
+MATH_VER = '12.3';  % change it to currently installed Wolfram Mathematica version
+if ismac
+    wstp_dir = '/Applications/Mathematica.app/Contents/SystemFiles/Links/WSTP/DeveloperKit/MacOSX-x86-64/CompilerAdditions';
+    mllib=fullfile(wstp_dir, 'wstp.framework');%'libWSTPi4.a');
+elseif isunix
+    wstp_dir = fullfile('/usr/local/Wolfram/Mathematica/',MATH_VER,'SystemFiles/Links/WSTP/DeveloperKit/Linux-x86-64/CompilerAdditions');
+    mllib=fullfile(wstp_dir, 'libWSTP64i4.so');
+elseif ispc 
+    wstp_dir = fullfile('C:∖Program Files∖Wolfram Research∖Mathematica∖',MATH_VER,'SystemFiles\Links\WSTP\DeveloperKit\Windows-x86-64\CompilerAdditions');
+    mllib=fullfile(wstp_dir, 'libWSTP64i4.lib');
+else
+    error('architecture not supported');
+end
+
+
+%%
+ % Copy wstp64i4.lib into toolbox directory or replace wstp64i4 with the appropriate library for your version of Mathematica
 
 %make command
-command=sprintf('mex -v -D__STDC__ -I%s %s %s', path2toolbox, 'math.c', mllib);
+command=sprintf('mex -v -I%s %s %s', wstp_dir, 'math.c', mllib);
 %compile
 eval(command)  
+
+%%
+
+testscript
